9f8c9715516ee35f8516bf48863478965ec9a841
eringrand <eringrand@gmail.com>
2025-09-16 15:32:03 -0400

    init csv change

--- R/scrape.R
+++ R/scrape.R
@@ -1,13 +1,33 @@
 library(httr2)
+library(jsonlite)
+library(lubridate)
 
-date <- Sys.Date() - 1
+# Set up what days we want data for
+start_date <- ymd("2025-09-10")
+today <- today()
 
-req <- request("https:/newsapi.org/v2/everything") |>
-  req_url_query(
-    q = '`"data science"`',
-    from = date,
-    pageSize = 10,
-    apiKey = Sys.getenv("NEWS_API_KEY")
-  )
+days_wanted <- seq(start_date, today, by = 1)
 
-req_perform(req, path = paste0("data/", date, ".json"))
+for(date in days_wanted) {
+  if(file.exists(glue::glue("csv/{date}.csv"))) {
+    next
+  }
+
+  req <- request("https:/newsapi.org/v2/everything") |>
+      req_url_query(
+        q = '`"data science"`',
+        from = date,
+        pageSize = 10,
+        apiKey = Sys.getenv("NEWS_API_KEY")
+      )
+
+    req_perform(req, path = paste0("data/", date, ".json"))
+
+    json_data_raw <- fromJSON(glue::glue("data/{date}.json"))
+
+    news_file <- json_data_raw$articles |>
+      as.data.frame()
+
+    # Write out today's data
+    write.csv(news_file, glue::glue("csv/{date}.csv"), row.names = FALSE)
+}
